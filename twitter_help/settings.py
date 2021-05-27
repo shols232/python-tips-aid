e633da6fad3e5815698b3ca526fc7082ac45603d
shols232 <akinsolaademola232@gmail.com>
2021-05-27 22:39:34 +0100

    added rest api

--- twitter_help/settings.py
+++ twitter_help/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'twitter_help.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# if DEBUG == 'TRUE':
+# if DEBUG:
 #     DATABASES = {
 #         'default': {
 #             'ENGINE': 'django.db.backends.postgresql_psycopg2',
